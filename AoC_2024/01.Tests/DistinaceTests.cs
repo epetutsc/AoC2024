8754e4c242a68de3ac4f6ccfd160a45e0c1ba1e4
Ewald Petutschnig <ewald.petutschnig@gmail.com>
2024-12-01 18:29:46 +0100

    day 1

---
+++ AoC_2024/01.Tests/DistinaceTests.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+
+namespace _01.Tests;
+
+public class DistinaceTests
+{
+    [Theory]
+    [InlineData(1, 3, 2)]
+    [InlineData(2, 3, 1)]
+    [InlineData(3, 3, 0)]
+    [InlineData(4, 3, 1)]
+    [InlineData(3, 5, 2)]
+    [InlineData(4, 9, 5)]
+    public void CanCalculateDistance(int first, int second, int expected)
+    {
+        // Arrange
+        var distance = new Distance();
+
+        // Act
+        var actual = distance.Calculate(first, second);
+
+        // Assert
+        actual.Should().Be(expected);
+    }
+
+    [Fact]
+    public void CanCalculateTotalDistance()
+    {
+        int[] column1 = [3, 4, 2, 1, 3, 3];
+        int[] column2 = [4, 3, 5, 3, 9, 3];
+
+        var distance = new Distance();
+        var total = distance.CalculateTotal(column1, column2);
+
+        total.Should().Be(11);
+    }
+}
---
+++ AoC_2024/01/Program.cs
@@ -0,0 +1,15 @@
+﻿using _01;
+using System.IO.Abstractions;
+
+const string File = @"C:\Users\ewald\Dropbox\Advent of Code\2024\1a\input.txt";
+
+var reader = new InputReader(new FileSystem());
+await reader.ReadFileAsync(File);
+var distance = new Distance();
+var totalDistaince = distance.CalculateTotal(reader.Column1, reader.Column2);
+
+var similarity = new Similarity(reader.Column2);
+var totalSimilarity = similarity.CalculateTotal(reader.Column1);
+
+Console.WriteLine(totalDistaince);
+Console.WriteLine(totalSimilarity);
