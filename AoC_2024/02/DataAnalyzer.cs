18d484d2af3e6760cc8562b512a523789c287a62
Ewald Petutschnig <ewald.petutschnig@gmail.com>
2024-12-02 23:39:44 +0100

    refactor, comments

--- AoC_2024/02/DataAnalyzer.cs
+++ AoC_2024/02/DataAnalyzer.cs
@@ -1,81 +1,87 @@
-﻿
+﻿namespace _02;
 
-
-
-
-namespace _02
+public class DataAnalyzer
 {
-    public class DataAnalyzer
+    /// <summary>
+    /// Check if the given report is safe. A report is safe if:
+    /// * all numbers are in the same direction
+    /// * all distances between numbers are 1, 2, or 3
+    /// * if the report is not safe, check if it can be made safe by removing one number
+    /// </summary>
+    public bool IsSafe(IReadOnlyList<int> report)
     {
-        public bool IsSafe(IReadOnlyList<int> report)
+        if (!IsSafeInternal(report))
         {
-            if (!IsSafeInternal(report))
-            {
-                return report
-                    .Select((_, i) => report.Take(i).Concat(report.Skip(i + 1)).ToArray())
-                    .Any(IsSafeInternal);
-            }
+            return report
+                // remove one item at a time and check if the rest is safe
+                .Select((_, i) => report.Take(i).Concat(report.Skip(i + 1)).ToArray())
+                .Any(IsSafeInternal);
+        }
 
+        return true;
+    }
+
+    /// <summary>
+    /// Check if the given report is safe. A report is safe if:
+    /// * all numbers are in the same direction
+    /// * all distances between numbers are 1, 2, or 3
+    /// </summary>
+    private static bool IsSafeInternal(IReadOnlyList<int> report)
+    {
+        if (report.Count < 2)
+        {
+            // a report with less than 2 numbers is always safe
             return true;
         }
 
-        private static bool IsSafeInternal(IReadOnlyList<int> report)
+        if (!AllSameDirection(report))
         {
-            if (report.Count < 2)
-            {
-                return true;
-            }
-
-            if (!AllSameDirection(report))
-            {
-                return false;
-            }
-
-            if (!AllDistancesAreOk(report))
-            {
-                return false;
-            }
-
-            return true;
+            // if the numbers are not in the same direction, the report is not safe
+            return false;
         }
 
-        private static bool AllDistancesAreOk(IReadOnlyList<int> report)
+        if (!AllDistancesAreOk(report))
         {
-            for (var i = 0; i < report.Count - 1; i++)
-            {
-                var distance = Math.Abs(report[i] - report[i + 1]);
-                if (distance is not (>= 1 and <= 3))
-                {
-                    return false;
-                }
-            }
-
-            return true;
+            // if the distances between numbers are not 1, 2, or 3, the report is not safe
+            return false;
         }
 
-        private static bool AllSameDirection(IReadOnlyList<int> report)
-        {
-            var direction = GetDirection(report[0], report[1]);
-            for (var i = 1; i < report.Count - 1; i++)
-            {
-                var dir = GetDirection(report[i], report[i + 1]);
-                if (dir != direction)
-                {
-                    return false;
-                }
-            }
+        return true;
+    }
 
-            return true;
-        }
+    /// <summary>
+    /// Check if all distances between numbers are 1, 2, or 3
+    /// </summary>
+    private static bool AllDistancesAreOk(IReadOnlyList<int> report)
+    {
+        return report
+            // calculate the distance between each number
+            .Zip(report.Skip(1), (a, b) => Math.Abs(a - b))
+            .All(distance => distance is >= 1 and <= 3);
+    }
 
-        private static int GetDirection(int v1, int v2)
-        {
-            return v1 - v2 > 0 ? -1 : 1;
-        }
+    /// <summary>
+    /// Check if all numbers are in the same direction
+    /// </summary>
+    private static bool AllSameDirection(IReadOnlyList<int> report)
+    {
+        var direction = GetDirection(report[0], report[1]);
+        return report.Skip(1)
+            // calculate the direction between each number
+            .Zip(report.Skip(2), GetDirection)
+            .All(dir => dir == direction);
+    }
 
-        public int CountSafe(IReadOnlyList<IReadOnlyList<int>> reports)
-        {
-            return reports.Count(report => IsSafe(report));
-        }
+    private static int GetDirection(int v1, int v2)
+    {
+        return v1 - v2 > 0 ? -1 : 1;
+    }
+
+    /// <summary>
+    /// Count the number of safe reports
+    /// </summary>
+    public int CountSafe(IReadOnlyList<IReadOnlyList<int>> reports)
+    {
+        return reports.Count(IsSafe);
     }
 }
