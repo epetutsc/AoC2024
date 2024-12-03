a513c8ecbc9783258e7bbbce232674c291283de1
Ewald Petutschnig (BCC.ICT) <ewald.petutschnig@oebb.at>
2024-12-03 23:15:01 +0100

    day 3 (mul, do, dont operations)

---
+++ AoC_2024/03/Program.cs
@@ -0,0 +1,10 @@
+﻿using _03;
+using System.IO.Abstractions;
+
+const string file = @"D:\dev\private\AoC2024\3a\input.txt";
+
+var reader = new InputReader(new FileSystem());
+var input = await reader.GetMultiplicationsAsync(file);
+var calculator = new Calculator(input);
+
+Console.WriteLine(calculator.AddMultiplications());
